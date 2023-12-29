bed51e997cb99c409bbcfe796b63e20f410a87bf
erikkindt <erikkindt@hotmail.com>
2023-12-29 10:28:56 +0100

    Set up and link component library

---
+++ apps/frontend-client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { Button } from "tuna-ui";
+
+function App() {
+  return (
+    <div>
+      <h1>My Frontend Client App</h1>
+      <Button label="Click me" />
+    </div>
+  );
+}
+
+export default App;
